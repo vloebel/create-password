b21b2fb7d780a625d29d9f47554c8325342764b8
Vicky Loebel <vickyloebel@gmail.com>
2020-12-30 20:36:56 -0700

    working on pwd generate function

--- GenPwdTest.js
+++ GenPwdTest.js
@@ -2,9 +2,9 @@ var passwordSourceString = '';
 var passwordString = '';
 
 // FUNCTION buildSourceStr
-// create a source array for the password generator
-// based on user specified length and including
-// the the user's choice of character types
+// Set up global passwordSourceString for the password generator
+// by including the user's choice of character types
+// Return the user's specified password length
 
 function buildSourceStr() {
   var specifyLengthPrompt = 'Specify password length by entering a number between ';
@@ -32,7 +32,6 @@ function buildSourceStr() {
     if (!selectionMade) alert('Please pick at least one  type to continue');
   }
   // Get the user-defined password length
- 
   var pwdLength = 0;
   specifyLengthPrompt = specifyLengthPrompt.concat(minPasswordLength, ' - ', maxPasswordLength);
   // prompt for a length between the boundary constants
@@ -40,46 +39,45 @@ function buildSourceStr() {
     pwdLength = Number(prompt(specifyLengthPrompt, defaultPasswordLength));
     console.log("pwd length is " + pwdLength);
   }
-  // create a source array of password characters that incluldes only
-  // the types chosen by the user above
+  // create a source array of password characters that only
+  // includes the types chosen by the user 
   
- // Initialize the length of the password source array
-  var srcStrLen = 0;
-
-  // add lowercase letters, if any
   if (useLower) {
     passwordSourceString = passwordSourceString.concat('abcdefghijklmnopqrstuvwxyz');
-    srcStrLen += 26;
   }
-  // add upper case, if any
+  
   if (useUpper) {
     passwordSourceString = passwordSourceString.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    srcStrLen += 26;
   }
-  // add numbers, if any
+
   if (useNum) {
     passwordSourceString = passwordSourceString.concat('A0123456789');
-    srcStrLen += 10;
   }
-  // add special characters, if any
+
   if (useSpecial) {
     passwordSourceString = passwordSourceString.concat('\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\{\[\}\]\|\\\:\;\"\'\<\,\>\.\?\/');
-    srcStrLen += 32;
   }
 
   console.log('source string is ', passwordSourceString);
   console.log('srcStrLen is ', srcStrLen);
 
-  // the source string array is global 
+  // return the desired password length 
   return (pwdLength);
 }
 
-// Given a string and its length (probably not even necessary)
-// create a new password of the desired lenght by selecting
-// characters from the source string at random
+// FUNCTION generatePwdStr (pwLength, pwSource)
+// Given the length of the desired password and
+// the (global) string containing valid characters
+// Pick characters at random until the password
+// is full
 
-function generatePwdStr(strLen, string) {
-  var ps = 'Default-pa$$word'; 
+function generatePwdStr(pwLength, pwSource) {
+    var ps = 'Default-pa$$word'; 
+  for (i = 0; i < pwLength; i++) {
+    //nextIndex = get a random number between 0- pwSource.length;
+    ps[i] = pwSource[nextIndex];
+
+  }
   return (ps);
 }
 
